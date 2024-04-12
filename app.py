a5ab98b808d560b25d41f521c6fff9fbb69ad902
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-04-12 11:21:10 +0530

    Create app.py
    Set up basic structure of the Dashboard.

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import necessary libraries 
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title="Dashboard",
+                   page_icon=":bar_chart:",
+                   layout="wide"
+                   )
+st.header('\n\n\nGlobal Superstore\n\n\n',f"[bold green]{header}[/]")
+st.subheader('Individual Coursework')
+
+
+
+
