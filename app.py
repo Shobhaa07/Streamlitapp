92f31d828d127e62d48b2183a81a58c42fa4e621
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-04-13 01:35:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from PIL import Image
 
-st.set_page_config(page_title="Dashboard",
-                   page_icon=":bar_chart:",
-                   layout="wide"
-                   )
-st.header('Global Superstore',f"[bold green]{header}[/]")
-st.subheader('Individual Coursework')
+# Setting page configuration
+st.set_page_config(
+    page_title="Global Superstore Dashboard",
+    page_icon=":bar_chart:",
+    layout="wide"
+)
 
 
 
