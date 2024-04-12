ddd2d3cf59d0ce4119b525c39e6adbd54369a583
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-04-12 12:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Dashboard",
                    page_icon=":bar_chart:",
                    layout="wide"
                    )
-st.header('\n\n\nGlobal Superstore\n\n\n',f"[bold green]{header}[/]")
+st.header('Global Superstore',f"[bold green]{header}[/]")
 st.subheader('Individual Coursework')
 
 
