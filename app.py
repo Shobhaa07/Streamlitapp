d02ffceb97d715fe3c7be2138288c26e6fd833b1
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-04-12 11:51:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Imports necessary libraries 
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
 
 st.set_page_config(page_title="Dashboard",
                    page_icon=":bar_chart:",
