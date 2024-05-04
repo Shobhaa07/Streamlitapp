4ffb2e5618d6d01cc3f77d62c1f55fc4ce4fac3d
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-05-04 21:16:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-# Setting page configuration
+
 st.set_page_config(
     page_title="Dashboard",
     page_icon=":bar_chart:",
