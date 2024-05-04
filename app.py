3917a2daabf679ca42b948a460184612b04be001
Shobhaa07 <165816352+Shobhaa07@users.noreply.github.com>
2024-05-05 02:03:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with col1:
 # Bar Chart
 with col2:
         fig_bar = px.bar(df, x='Ship Mode', y='Shipping Cost', title='Bar Chart')
-        fig_bar.update_traces(marker=dict(color='blue')) 
+        fig_bar.update_traces(marker=dict(color='black')) 
         st.plotly_chart(fig_bar, use_container_width=True)
 
 # Donut Chart
